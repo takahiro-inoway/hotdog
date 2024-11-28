6e45015db6a61483c0171f1ce825c5d7862000b6
takahiro-inoway <81753923+takahiro-inoway@users.noreply.github.com>
2024-11-28 20:39:04 +0900

    Add files via upload

---
+++ echo hotel/songs.js
@@ -0,0 +1,98 @@
+const SONGS = [
+  {
+    id: 1,
+    title: "fast sax MAX !",
+    genre: "JAZZ",
+    recommended: true
+  },
+  {
+    id: 2,
+    title: "fast sax level",
+    genre: "JAZZ",
+    recommended: false
+  },
+  {
+    id: 3,
+    title: "fast sax story",
+    genre: "JAZZ",
+    recommended: false
+  },
+  {
+    id: 4,
+    title: "fast sax story history",
+    genre: "JAZZ",
+    recommended: false
+  },
+  {
+    id: 5,
+    title: "Electric fast sax !",
+    genre: "JAZZ",
+    recommended: false
+  },
+  {
+    id: 6,
+    title: "Tric fast sax !",
+    genre: "JAZZ",
+    recommended: false
+  },
+  {
+    id: 7,
+    title: "piano sax faster",
+    genre: "JAZZ",
+    recommended: false
+  },
+  {
+    id: 8,
+    title: "Very fast sax and piano",
+    genre: "JAZZ",
+    recommended: false
+  },
+  {
+    id: 9,
+    title: "Jump ! Kick ! Rowling !",
+    genre: "JAZZ",
+    recommended: true
+  },
+  {
+    id: 10,
+    title: "Jam ! Cockie ! Cake !",
+    genre: "JAZZ",
+    recommended: false
+  },
+  {
+    id: 11,
+    title: "Morning Song",
+    genre: "JAZZ",
+    recommended: true
+  },
+  {
+    id: 12,
+    title: "Morning Best",
+    genre: "JAZZ",
+    recommended: false
+  },
+  {
+    id: 13,
+    title: "IMPACT JAZZ",
+    genre: "JAZZ",
+    recommended: false
+  },
+  {
+    id: 14,
+    title: "ACT JAZZ",
+    genre: "JAZZ",
+    recommended: false
+  },
+  {
+    id: 15,
+    title: "Danger Shot High",
+    genre: "JAZZ",
+    recommended: true
+  },
+  {
+    id: 16,
+    title: "Danger SH",
+    genre: "JAZZ",
+    recommended: false
+  },
+];
